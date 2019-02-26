8a47b52724c0eb9b8dcdb394cb61a3bd8ccc3466
ParkTS <qkrxotj1992@gmail.com>
2019-02-26 09:37:50 +0900

    20190226 09:37 sql insert문 좌표추가

--- sabang/WebContent/sql/insert.sql
+++ sabang/WebContent/sql/insert.sql
@@ -1,47 +1,47 @@
 -- house_info
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('O001', 'o', '월세', '가성비 최고 풀 옵션 원룸', '서울시 서대문구 창천동', null, null, '19.83m²', 4, 2, '1', '1', 1, 'oimage1', '인근편의시설 확보 좋습니다. 주택가 초입에 위치하고 있어요 :)', '20190201');
+values ('O001', 'o', '월세', '가성비 최고 풀 옵션 원룸', '서울시 서대문구 창천동', '37.55865382332561', '126.93427968164825', '19.83m²', 4, 2, '1', '1', 1, 'oimage1', '인근편의시설 확보 좋습니다. 주택가 초입에 위치하고 있어요 :)', '20190201');
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('O002', 'o', '월세', '연대 인근 깔끔한 건물', '서울시 서대문구 연희동', null, null, '16.53m²', 3, 2, '1', '1', 0, 'oimage2', '거의 신축급 깔끔한 방! 저렴한 가격의 매물을 찾는 직장인과 대학생에게 안성맞춤!', '20190206');
+values ('O002', 'o', '월세', '연대 인근 깔끔한 건물', '서울시 서대문구 연희동', '37.564022444565694', '126.93195238268775', '16.53m²', 3, 2, '1', '1', 0, 'oimage2', '거의 신축급 깔끔한 방! 저렴한 가격의 매물을 찾는 직장인과 대학생에게 안성맞춤!', '20190206');
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('O003', 'o', '월세', '분위기있는 신축 옥탑', '서울시 서대문구 연희동', null, null, '33.06m²', 4, 3, '1', '1', 3, 'oimage3', '넓은 마당 사용으로 공간 활용도 최강, 풀옵션 가성비 갑', sysdate);
+values ('O003', 'o', '월세', '분위기있는 신축 옥탑', '서울시 서대문구 연희동', '37.56450906563715', '126.93209681948031', '33.06m²', 4, 3, '1', '1', 3, 'oimage3', '넓은 마당 사용으로 공간 활용도 최강, 풀옵션 가성비 갑', sysdate);
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('O004', 'o', '월세', '꿀남향 신축급 이중보안', '서울시 서대문구 창천동', null, null, '23.14m²', 7, 3, '1', '1', 2, 'oimage4', '명물거리 근처! 편의시설 모두 갖춰 생활하세요. 이중보안으로 안전해요', '20190211');
+values ('O004', 'o', '월세', '꿀남향 신축급 이중보안', '서울시 서대문구 창천동', '37.5588707736284', '126.93556972620135', '23.14m²', 7, 3, '1', '1', 2, 'oimage4', '명물거리 근처! 편의시설 모두 갖춰 생활하세요. 이중보안으로 안전해요', '20190211');
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('O005', 'o', '월세', '특급추천 채광짱 샤워부스', '서울시 서대문구 대신동', null, null, '19.83m²', 5, 2, '1', '1', 2, 'oimage5', '1층에 경비원 있습니다 무조건 안전이 최고지요. 인기 짱 매물 서두르세요 :)', '20190203');
+values ('O005', 'o', '월세', '특급추천 채광짱 샤워부스', '서울시 서대문구 대신동', '37.564800437357626', '126.94487315886799', '19.83m²', 5, 2, '1', '1', 2, 'oimage5', '1층에 경비원 있습니다 무조건 안전이 최고지요. 인기 짱 매물 서두르세요 :)', '20190203');
 
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('T001', 't', '월세', '신촌역 도보5분 신축급 투룸', '서울시 서대문구 창천동', null, null, '46.28m²', 6, 4, '2', '1', 0, 'timage1', '신촌역 도보5분거리 주택가초입이며 싱크대,도배새로교체하여 깔끔합니다.', '20190203');
+values ('T001', 't', '월세', '신촌역 도보5분 신축급 투룸', '서울시 서대문구 창천동', '37.55860019936876', '126.93506744837961', '46.28m²', 6, 4, '2', '1', 0, 'timage1', '신촌역 도보5분거리 주택가초입이며 싱크대,도배새로교체하여 깔끔합니다.', '20190203');
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('T002', 't', '월세', '넓은 투룸 연세대 5분거리 보증금 조정 가능', '서울시 서대문구 창천동', null, null, '49.58m²', 4, 1, '2', '1', 0, 'timage2', '채광 너무 좋아요~ 주택가 초입으로 밤에도 안전해요.', sysdate);
+values ('T002', 't', '월세', '넓은 투룸 연세대 5분거리 보증금 조정 가능', '서울시 서대문구 창천동', '37.5590685728757', '126.93480446707945', '49.58m²', 4, 1, '2', '1', 0, 'timage2', '채광 너무 좋아요~ 주택가 초입으로 밤에도 안전해요.', sysdate);
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('T003', 't', '월세', '신촌역 연세대 도보 5분 쓰리룸', '서울시 서대문구 창천동', null, null, '72.72m²', 5, 5, '3', '1', 0, 'timage3', '직접보시면 더욱더 좋습니다.문의주세요', sysdate);
+values ('T003', 't', '월세', '신촌역 연세대 도보 5분 쓰리룸', '서울시 서대문구 창천동', '37.55927684119469', '126.93343708170717', '72.72m²', 5, 5, '3', '1', 0, 'timage3', '직접보시면 더욱더 좋습니다.문의주세요', sysdate);
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('T004', 't', '월세', '서강대 후문 인근 올수리 풀옵션 쓰리룸', '서울시 마포구 대흥동', null, null,'66.11m²', 3, 2, '3', '1', 0, 'timage4', '대로변으로 안전하며 풀옵션 올수리로 몸만 오시면 됩니다', sysdate);
+values ('T004', 't', '월세', '서강대 후문 인근 올수리 풀옵션 쓰리룸', '서울시 마포구 대흥동', '37.55498893879162', '126.94186094076593','66.11m²', 3, 2, '3', '1', 0, 'timage4', '대로변으로 안전하며 풀옵션 올수리로 몸만 오시면 됩니다', sysdate);
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('T005', 't', '전세', '위치 끝내주는 가성비 투룸', '서울시 마포구 염리동', null, null, '46.28m²', 5, 4, '1', '1', 0, 'timage5', '방범이 잘되어있어서 여성분들에게도 추천! 빌트인 옷장으로 시원시원한 내부!^^', '20190218');
+values ('T005', 't', '전세', '위치 끝내주는 가성비 투룸', '서울시 마포구 염리동', '37.555675951770816', '126.94663633678059', '46.28m²', 5, 4, '1', '1', 0, 'timage5', '방범이 잘되어있어서 여성분들에게도 추천! 빌트인 옷장으로 시원시원한 내부!^^', '20190218');
 
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('F001', 'f', '월세', '신촌역 초인근 주변 산책로와 쇼핑단지 조성 굿', '서울특별시 마포구 노고산동', null, null, '13.52㎡', 11, 15, '1', '1', 0, 'fimage1', '방범이 잘되어있어서 여성분들에게도 추천! 빌트인 옷장으로 시원시원한 내부!^^', sysdate);
+values ('F001', 'f', '월세', '신촌역 초인근 주변 산책로와 쇼핑단지 조성 굿', '서울특별시 마포구 노고산동', '37.55544536289714', '126.93931192255577', '13.52㎡', 11, 15, '1', '1', 0, 'fimage1', '방범이 잘되어있어서 여성분들에게도 추천! 빌트인 옷장으로 시원시원한 내부!^^', sysdate);
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('F002', 'f', '월세', '신촌대로 신축 오피스텔 반전세★ 폴옵션 복층 ★', '서울특별시 마포구 노고산동', null, null, '19.99㎡', 13, 4, '1', '1', 0, 'fimage2', '드라마에 나올법한 주차도 가능하고 교통도 좋은 복층 집', '20190202');
+values ('F002', 'f', '월세', '신촌대로 신축 오피스텔 반전세★ 폴옵션 복층 ★', '서울특별시 마포구 노고산동', '37.55522967243774', '126.94038950455885', '19.99㎡', 13, 4, '1', '1', 0, 'fimage2', '드라마에 나올법한 주차도 가능하고 교통도 좋은 복층 집', '20190202');
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('F003', 'f', '월세', '?바닥 교체, 풀옵션  연대,이대,서강대 가까운 오피스텔?', '서울특별시 마포구 노고산동', null, null, '31.41㎡', 15, 9, '1', '1', 0, 'fimage3', '신촌역1분거리, 위치 최고 좋은 방', '20190202');
+values ('F003', 'f', '월세', '?바닥 교체, 풀옵션  연대,이대,서강대 가까운 오피스텔?', '서울특별시 마포구 노고산동', '37.55497398302118', '126.93397961072301', '31.41㎡', 15, 9, '1', '1', 0, 'fimage3', '신촌역1분거리, 위치 최고 좋은 방', '20190202');
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('F004', 'f', '월세', '신촌역 바로앞.대로변 오피스텔', '서울특별시 서대문구 창천동', null, null, '26.67㎡', 15, 10, '1', '1', 0, 'fimage4', '깔끔한 오피스텔..학생이나 젊은층 좋아하시는 물건예요.', '20190215');
+values ('F004', 'f', '월세', '신촌역 바로앞.대로변 오피스텔', '서울특별시 서대문구 창천동', '37.5562057974519', '126.93929772492868', '26.67㎡', 15, 10, '1', '1', 0, 'fimage4', '깔끔한 오피스텔..학생이나 젊은층 좋아하시는 물건예요.', '20190215');
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('F005', 'f', '월세', '◐신촌역 도보2분, 대로변, 오픈형 구조, 빌트인, 주차여유◑', '서울특별시 마포구 노고산동', null, null, '19.53㎡ ', 14, 10, '1', '1', 0, 'fimage5', '◇2호선(신촌역), 경의선(서강역), 신공항선(홍대역)의 트리플역세권', '20190215');
+values ('F005', 'f', '월세', '◐신촌역 도보2분, 대로변, 오픈형 구조, 빌트인, 주차여유◑', '서울특별시 마포구 노고산동', '37.55605167068116', '126.94096377501103', '19.53㎡ ', 14, 10, '1', '1', 0, 'fimage5', '◇2호선(신촌역), 경의선(서강역), 신공항선(홍대역)의 트리플역세권', '20190215');
 
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('P001', 'p', '월세', '싱크대수리한방/방1개 분리형/신촌유일주상복합아파트/베란다있음', '서울특별시 마포구 노고산동', null, null, '38.02㎡ ', 19, 8, '1', '1', 0, 'pimage1', '신촌 초역세권에 있는 주상복합아파트입니다.', '20190207');
+values ('P001', 'p', '월세', '싱크대수리한방/방1개 분리형/신촌유일주상복합아파트/베란다있음', '서울특별시 마포구 노고산동', '37.55569169876663', '126.94182417920896', '38.02㎡ ', 19, 8, '1', '1', 0, 'pimage1', '신촌 초역세권에 있는 주상복합아파트입니다.', '20190207');
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('P002', 'p', '월세', '연세대 도보 바로앞 14평형 저렴한 월세 / 남향 / 즉시입주가능', '서울특별시 서대문구 대신동', null, null, '29.03㎡ ', 15, 14, '2', '1', 0, 'pimage2', '2호선 신촌역 도보 5분, 이대역 도보 3분 거리에 위치한 포스빌아파트 14평형 월세입니다', sysdate);
+values ('P002', 'p', '월세', '연세대 도보 바로앞 14평형 저렴한 월세 / 남향 / 즉시입주가능', '서울특별시 서대문구 대신동', '37.563916970608894', '126.94383249723303', '29.03㎡ ', 15, 14, '2', '1', 0, 'pimage2', '2호선 신촌역 도보 5분, 이대역 도보 3분 거리에 위치한 포스빌아파트 14평형 월세입니다', sysdate);
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('P003', 'p', '월세', '연세대 도보 3분거리 풀 옵션 아파트. 연세대 정문 앞', '서울특별시 서대문구 연희동', null, null, '34.64㎡  ', 19, 15, '2', '1', 0, 'pimage3', '신촌역 3 분 거리 풀 옵션 아파트 방2개 서강대 정문 앞 연대. 이대 홍대. 현대 백화점. 세브란스 병원 근교', '20190207');
+values ('P003', 'p', '월세', '연세대 도보 3분거리 풀 옵션 아파트. 연세대 정문 앞', '서울특별시 서대문구 연희동', '37.564106044648376', '126.93319282998453', '34.64㎡  ', 19, 15, '2', '1', 0, 'pimage3', '연세대 3 분 거리 풀 옵션 아파트 방2개 연세대 정문 앞 연대. 이대 홍대. 현대 백화점. 세브란스 병원 근교', '20190207');
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('P004', 'p', '월세', '연대 앞 북향  넓은 복층 주상복합 아파트', '서울특별시 서대문구 연희동', null, null, '32.34㎡', 15, 11, '2', '1', 0, 'pimage4', '2호선 신촌역 이대역 도보 5분거리에 위치한 포스빌 아파트 북향 방이나왔어요.', '20190206');
+values ('P004', 'p', '월세', '연대 앞 북향  넓은 복층 주상복합 아파트', '서울특별시 서대문구 연희동', '37.563767002879196', '126.93272227930592', '32.34㎡', 15, 11, '2', '1', 0, 'pimage4', '연세대 도보 5분거리에 위치한 포스빌 아파트 북향 방이나왔어요.', '20190206');
 insert into house_info (hcode, htype, rtype, hname, addr, coordx, coordy, area, whlflr, flr, room, batr, cntwish, himage, hetc, registerdate)
-values ('P005', 'p', '전세', '올수리 깔끔한집 홍대입구 공원길옆', '서울특별시 마포구 신수동', null, null, '59.85㎡ ', 14, 10, '3', '1', 0, 'pimage5', '초역세권, 쾌적한 공원, 한강 조망, 평지 아파트 직접 와보시면 알 거예요', '20190210');
+values ('P005', 'p', '전세', '올수리 깔끔한집 홍대입구 공원길옆', '서울특별시 마포구 동교동', '37.55721705032405', '126.93012512025646', '59.85㎡ ', 14, 10, '3', '1', 0, 'pimage5', '초역세권, 홍대입구 도보 5분거리, 홍대 걸어서 10분거리', '20190210');
 
 
 
